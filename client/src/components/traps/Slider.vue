93d9d5e62343599e05509057cc5030d67d7f46a9
amelie laurent <amelielaurent38@gmail.com>
2019-06-12 21:01:14 +0200

    Add content to slider

--- client/src/components/traps/Slider.vue
+++ client/src/components/traps/Slider.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="slider__container">
-      <carousel class="slider__carousel" :per-page="1" :mouse-drag="false">
+      <carousel class="slider__carousel" :per-page="1" :mouse-drag="false" :paginationColor="black" :paginationActiveColor="white" >
         <slide class="slider__slide">
           <img class="slider__image" :src="harassment"/>
         </slide>
@@ -14,17 +14,23 @@
           <img class="slider__image" :src="harassment"/>
         </slide>
       </carousel>
+      <div class="slider__group">
+            <h2 class="slider__title"> Holidays in Miami </h2>
+            <div class="slider__separate"></div>
+            <Comment name="Jean Lefert" date="Il y a 2h" description="Trop cool cette photo !"/>
+            <Comment name="Pauline Bernard" date="Il y a 3 jours" description="Ah t'es trop belle !"/>
+        </div>
     </div>
 </template>
 
 <script>
-
-    import VueCarousel from 'vue-carousel';
     import { Carousel, Slide } from 'vue-carousel';
     import Harassment from '@/assets/img/harassment.jpg';
+    import Comment from '@/components/traps/post/Comment';
+
     export default {
         name: "Slider",
-        components: {  VueCarousel, Carousel, Slide },
+        components: {  Carousel, Slide, Comment },
         data: () => ({
           harassment: Harassment
         }),
@@ -35,24 +41,37 @@
 </script>
 
 <style scoped lang="scss">
-    .slider {
-      &__container {
-        border: 1.5px solid $white;
-        .VueCarousel-dot-container {
-          margin: 0;
-        }
-      }
-      &__carousel {
-        &.VueCarousel {
-           .VueCarousel-pagination{
-            margin-top: -6rem !important;
-            z-index: 5 !important;
-          }
-        }
-      }
-      &__image {
-       width: 100%;
-      }
-
+.slider {
+  &__container {
+    border: 1.5px solid $white;
+    .VueCarousel-dot-container {
+      margin: 0;
     }
+  }
+  &__carousel {
+    &.VueCarousel {
+        .VueCarousel-pagination{
+        margin-top: -6rem !important;
+        z-index: 5 !important;
+      }
+    }
+  }
+  &__image {
+    width: 100%;
+  }
+  &__group {
+    padding: 0 7rem 3.8rem 7rem;
+  }
+  &__title {
+    font-size: 1.6rem;
+    margin-bottom: 2rem;
+    margin-top: 2rem;
+  }
+  &__separate {
+    height: 1px;
+    background: grey;
+    width: 46rem;
+    margin-bottom: 1.7rem;
+  }
+}
 </style>
