b6a5e99b97c944cb60652ea1cdd63cdd16e0a7ce
amelie laurent <amelielaurent38@gmail.com>
2019-06-12 21:45:26 +0200

    Changes on modal story

--- client/src/store/modules/stories.js
+++ client/src/store/modules/stories.js
@@ -1,16 +1,16 @@
 const state = {
   allStories : [{
-    src: "https://picsum.photos/300/400",
+    src: "https://picsum.photos/300/500",
     name: "Boudon",
     description: "test",
     delay: 1500,
   },{
-      src: "https://picsum.photos/500/300",
+      src: "https://picsum.photos/300/500",
       name: "Barbie",
       description: "test2",
       delay: 1500,
   },{
-      src: "https://picsum.photos/200/400",
+      src: "https://picsum.photos/300/500",
       name: "Pourto",
       description: "test3",
       delay: 1500,
