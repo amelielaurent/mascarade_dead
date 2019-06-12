fcafab1e03eab981c9e7cc518c77f0dedad21190
amelie laurent <amelielaurent38@gmail.com>
2019-06-12 20:28:46 +0200

    Add slider

---
+++ client/src/components/traps/Slider.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="slider__container">
+      <carousel class="slider__carousel" :per-page="1" :mouse-drag="false">
+        <slide class="slider__slide">
+          <img class="slider__image" :src="harassment"/>
+        </slide>
+        <slide>
+          <img class="slider__image" :src="harassment"/>
+        </slide>
+        <slide>
+          <img class="slider__image" :src="harassment"/>
+        </slide>
+        <slide>
+          <img class="slider__image" :src="harassment"/>
+        </slide>
+      </carousel>
+    </div>
+</template>
+
+<script>
+
+    import VueCarousel from 'vue-carousel';
+    import { Carousel, Slide } from 'vue-carousel';
+    import Harassment from '@/assets/img/harassment.jpg';
+    export default {
+        name: "Slider",
+        components: {  VueCarousel, Carousel, Slide },
+        data: () => ({
+          harassment: Harassment
+        }),
+        methods: {
+           
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+    .slider {
+      &__container {
+        border: 1.5px solid $white;
+        .VueCarousel-dot-container {
+          margin: 0;
+        }
+      }
+      &__carousel {
+        &.VueCarousel {
+           .VueCarousel-pagination{
+            margin-top: -6rem !important;
+            z-index: 5 !important;
+          }
+        }
+      }
+      &__image {
+       width: 100%;
+      }
+
+    }
+</style>
